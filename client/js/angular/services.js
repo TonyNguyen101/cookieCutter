0a48a3c6fa0fa75cfa1cc94f29b7182ae14fe454
Anthony Nguyen <TonyNguyen@Anthonys-MacBook-Pro.local>
2015-10-01 20:55:33 -0700

    almost fixed create page: need better forms

--- client/js/angular/services.js
+++ client/js/angular/services.js
@@ -8,101 +8,101 @@ app.factory('Recipe', function () {
 			comments: [],	
 			borrowers: [],
 			allIngredients: [
-			// {
-			// 	ingredientName: 'all purpose flour',				
-	 	// 		imperialQuantity: 2,
-			// 	imperialUnits: "cup",
-			// 	description: "be sure to use all purpose flour as compared to bread, cake, or self-rising",
-			// 	comments: []
-			// },{
-			// 	ingredientName: 'chocolate chips',
-	 	// 		imperialQuantity: 4.5,
-			// 	imperialUnits: "oz",
-			// 	description: "You can also use chocloate chunks",
-			// 	comments: []
-			// },{
-			// 	ingredientName: 'unsalted butter',
-	 	// 		imperialQuantity: 4.5,
-			// 	imperialUnits: "stick",
-			// 	description: "organic prefered",
-			// 	comments: []
-			// },{
-			// 	ingredientName: 'granulated sugar',	
-	 	// 		imperialQuantity: 1.5,
-			// 	imperialUnits: "cup",
-			// 	description: "sifted",
-			// 	comments: []
-			// }  
+			{
+				ingredientName: 'all purpose flour',				
+	 			imperialQuantity: 2,
+				imperialUnits: "cup",
+				description: "be sure to use all purpose flour as compared to bread, cake, or self-rising",
+				comments: []
+			},{
+				ingredientName: 'chocolate chips',
+	 			imperialQuantity: 4.5,
+				imperialUnits: "oz",
+				description: "You can also use chocloate chunks",
+				comments: []
+			},{
+				ingredientName: 'unsalted butter',
+	 			imperialQuantity: 4.5,
+				imperialUnits: "stick",
+				description: "organic prefered",
+				comments: []
+			},{
+				ingredientName: 'granulated sugar',	
+	 			imperialQuantity: 1.5,
+				imperialUnits: "cup",
+				description: "sifted",
+				comments: []
+			}  
 			],
 			vessels: [
-			// {
-			// 	vesselName: 'small mixing bowl',
-			// 	formVisible: false,
-			// 	description: "any bowl will do",
-			// 	comments: [],
-			// 	actions: [
-			// 	{
-			// 		actionName: "mix in",
-			// 		formVisible: false,
-			// 		time: 7,
-			// 		description: "Slowly, one ingredient at a time",
-			// 		comments: [],
-			// 		ingredients: [
-			// 		{
-			// 			ingredientName: 'chocolate chips',
-	  //    			imperialQuantity: 4.5,
-			// 			imperialUnits: "oz",
-			// 			description: "You can also use chocloate chunks",
-			// 			comments: []
-			// 		},{
-			// 			ingredientName: 'unsalted butter',
-	  //    			imperialQuantity: 4.5,
-			// 			imperialUnits: "stick",
-			// 			description: "organic prefered",
-			// 			comments: []
-			// 		}
-			// 		]
-			// 	}
-			// 	]
-			// },{
-			// 	vesselName: "transfer one by one",
-			// 	description: "further details",
-			// 	transition: true
-			// },{
-			// 	vesselName: 'large mixing bowl',
-			// 	formVisible: false,
-			// 	description: "can use an auto mixing bowl",
-			// 	comments: [],
-			// 	actions: [
-			// 	{
-			// 		actionName: "slowly mix in",
-			// 		formVisible: false,
-			// 		time: 10,
-			// 		description: "You can use a auto mixer",
-			// 		comments: [],
-			// 		ingredients: [{
-			// 			ingredientName: 'granulated sugar',	
-	  //    			imperialQuantity: 1.5,
-			// 			imperialUnits: "cup",
-			// 			description: "sifted",
-			// 			comments: []
-			// 		}]
-			// 	},{ 
-			// 		actionName: "blend in",
-			// 		formVisible: false,
-			// 		time: 20,
-			// 		description: "use a whisk or wooden spoon",
-			// 		comments: [],
-			// 		ingredients: [{
-			// 			ingredientName: 'all purpose flour',				
-	  //    			imperialQuantity: 2,
-			// 			imperialUnits: "cup",
-			// 			description: "be sure to use all purpose flour as compared to bread, cake, or self-rising",
-			// 			comments: []
-			// 		}]
-			// 	}
-			// 	]
-			// }
+			{
+				vesselName: 'small mixing bowl',
+				formVisible: false,
+				description: "any bowl will do",
+				comments: [],
+				actions: [
+				{
+					actionName: "mix in",
+					formVisible: false,
+					time: 7,
+					description: "Slowly, one ingredient at a time",
+					comments: [],
+					ingredients: [
+					{
+						ingredientName: 'chocolate chips',
+	     			imperialQuantity: 4.5,
+						imperialUnits: "oz",
+						description: "You can also use chocloate chunks",
+						comments: []
+					},{
+						ingredientName: 'unsalted butter',
+	     			imperialQuantity: 4.5,
+						imperialUnits: "stick",
+						description: "organic prefered",
+						comments: []
+					}
+					]
+				}
+				]
+			},{
+				vesselName: "transfer one by one",
+				description: "further details",
+				transition: true
+			},{
+				vesselName: 'large mixing bowl',
+				formVisible: false,
+				description: "can use an auto mixing bowl",
+				comments: [],
+				actions: [
+				{
+					actionName: "slowly mix in",
+					formVisible: false,
+					time: 10,
+					description: "You can use a auto mixer",
+					comments: [],
+					ingredients: [{
+						ingredientName: 'granulated sugar',	
+	     			imperialQuantity: 1.5,
+						imperialUnits: "cup",
+						description: "sifted",
+						comments: []
+					}]
+				},{ 
+					actionName: "blend in",
+					formVisible: false,
+					time: 20,
+					description: "use a whisk or wooden spoon",
+					comments: [],
+					ingredients: [{
+						ingredientName: 'all purpose flour',				
+	     			imperialQuantity: 2,
+						imperialUnits: "cup",
+						description: "be sure to use all purpose flour as compared to bread, cake, or self-rising",
+						comments: []
+					}]
+				}
+				]
+			}
 			]
 		},
 		updateRecipe: function (replacementRecipe) {
